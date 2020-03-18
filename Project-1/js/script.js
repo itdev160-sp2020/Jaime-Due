5836dc713760bac6bed28fca766306f36f8c59e2
jaimedue <braxtonkeil@Braxtons-MacBook-Pro.local>
2020-03-17 23:24:55 -0500

    Completed Project 1

---
+++ Project-1/js/script.js
@@ -0,0 +1,9 @@
+var scroll="scroll down to see the cocktails",
+    scrollEl=document.getElementById("scroll");
+
+scrollEl.textContent = scroll;
+
+var scroll2="scroll down to see the liquor chart",
+    scroll2El=document.getElementById("scroll2");
+
+scroll2El.textContent = scroll2;
