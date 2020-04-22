882e197417c33b7d14a501adcc869134e6cc9426
jaimedue <braxtonkeil@Braxtons-MacBook-Pro.local>
2020-04-22 15:11:54 -0500

    Completed Activity 6

---
+++ activity-6/js/scripts.js
@@ -0,0 +1,65 @@
+//Messages array
+var messages = [];
+
+var messageType = {
+    out: 'out-message',
+    in: 'in-message',
+    unknown: 'unknown-message'
+};
+
+function Message (type, user, message) {
+    this.type = type;
+    this.user = user;
+    this.message = message;
+}
+
+function createMessageElement(message) {
+    var messageText = document.createTextNode(
+        message.user + ': ' + message.message
+    );
+
+    var messageEl = document.createElement('div');
+    messageEl.appendChild(messageText);
+
+    messageEl.className = message.type;
+    
+    return messageEl;
+}
+
+function addMessageHandler(event) {
+    var user, type;
+    var messageInput = document.getElementById("message-input");
+    var messageContainerEl = document.getElementById("message-container");
+
+    switch(event.target.id) {
+        case 'send-button':
+            user = 'Chatter_1';
+            type = messageType.out;
+            break;
+        case 'reply-button':
+            user = 'Chatter_2';
+            type = messageType.in;
+            break;
+        default:
+            user = 'unknown';
+            type = messageType.unknown;
+    }
+
+    if (messageInput.value !='') {
+        var message = new Message(type, user, messageInput.value);
+        messages.push(message);
+
+        var el = createMessageElement(message);
+
+        messageContainerEl.appendChild(el);
+
+        messageInput.value = '';
+    }
+}
+
+var init = function() {
+    document.getElementById('send-button').onclick = addMessageHandler;
+    document.getElementById('reply-button').onclick = addMessageHandler;
+};
+
+init();
